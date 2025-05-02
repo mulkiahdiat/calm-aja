dbd800d7cb9fb70fffc7ba4a14f7d8752be173d2
mulkiahdiat <ngeteshosting@gmail.com>
2025-05-02 13:19:52 -0300

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,26 +1,26 @@
-window.CMS.init({
-  config: {
-    backend: {
-      name: "github",
-      repo: "mulkiahdiat/calm-aja",
-      branch: "main",
-      auth_type: "oauth",
-    },
-    media_folder: "public/images/uploads",
-    public_folder: "/images/uploads",
-    collections: [
-      {
-        name: "posts",
-        label: "Artikel",
-        folder: "posts",
-        create: true,
-        slug: "{{slug}}",
-        fields: [
-          { label: "Judul", name: "title", widget: "string" },
-          { label: "Tanggal", name: "date", widget: "datetime" },
-          { label: "Konten", name: "body", widget: "markdown" }
-        ]
-      }
-    ]
-  }
+window.CMS_MANUAL_INIT = true;
+
+CMS.init({
+  backend: {
+    name: "github",
+    repo: "mulkiahdiat/calm-aja", // your GitHub repo
+    branch: "main",
+    site_domain: "splendorous-pie-8b9224.netlify.app"
+  },
+  media_folder: "public/assets",
+  public_folder: "/assets",
+  collections: [
+    {
+      name: "posts",
+      label: "Posts",
+      folder: "posts",
+      create: true,
+      slug: "{{slug}}",
+      fields: [
+        { label: "Title", name: "title", widget: "string" },
+        { label: "Date", name: "date", widget: "datetime" },
+        { label: "Body", name: "body", widget: "markdown" }
+      ]
+    }
+  ]
 });
